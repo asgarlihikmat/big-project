0aeb6494c55de7994b285dc3d2e2e4aa8d86661a
Hikmat Asgarli <hikmatasgarli@gmail.com>
2023-05-14 18:21:17 +0400

    MM

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,10 +1,6 @@
 
 
-<<<<<<< HEAD
-//Magic String   ====
-=======
 //Magic String ~~~~~~~
->>>>>>> main
 
 export const CHANGE_CATEGORY = "CHANGE_CATEGORY"
 export const GET_CATEGORIES_SUCCESS = "GET_CATEGORIES_SUCCESS"
